7c69b560cf6ac9f7955de061cb2c71ffdf533629
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-09-02 23:30:13 +0530

    Explore Card Base structure

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { motion } from "framer-motion";
+import { useState } from "react";
 import styles from '../styles';
 import {staggerContainer} from '../utils/motion';
 import {ExploreCard, TitleText, TypingText } from "../components";
 import { exploreWorlds } from "../constants";
 
-const Explore = () => (
+const Explore = () => {
+  
+  const [active, setActive] = useState('world-2');
+
+  return(
   <section className={`${styles.paddings}`} id='explore'>
     <motion.div
       variants={staggerContainer}
@@ -24,8 +29,20 @@ const Explore = () => (
         title ={<>Choose the world you want<br className="md:block hidden"/>to explore</>}
         textStyles='text-center'
       />
+
+      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+        {exploreWorlds.map((world, index) => (
+          <ExploreCard 
+            key={world.id}
+            {...world}
+            index={index}
+            active={active}
+            handleClick = {setActive}
+          />
+        ))}
+      </div>
     </motion.div>
   </section>
-);
+)};
 
 export default Explore;
