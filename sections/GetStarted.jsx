a7dbb51b8ad73723280357aae8533672c94ddc74
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-09-03 23:23:51 +0530

    Get started section started

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,31 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from '../styles';
+import {staggerContainer, fadeIn, planetVariants} from '../utils/motion';
+import {StartSteps, TitleText, TypingText } from "../components";
+import { startingFeatures } from "../constants";
+
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView='show'
+      viewport={{once: false, amount:0.25}}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={planetVariants('left')}
+        className={`${styles.flexCenter} flex-1`} 
+      >
+        <img
+          src="/get-started.png"
+          alt="Get Started"
+          className="w-[80%] h-[80%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
   </section>
 );
 
