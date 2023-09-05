d746d88f65ddd70dc7e2c89619b1da9bd9d2b45c
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-09-05 20:58:51 +0530

    get started part implemented

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -22,8 +22,30 @@ const GetStarted = () => (
         <img
           src="/get-started.png"
           alt="Get Started"
-          className="w-[80%] h-[80%] object-contain"
+          className="w-[90%] h-[90%] object-contain"
         />
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] flex flex-col justify-center"
+      >
+        <TypingText
+          title='| How metaverse works'
+        />
+        <TitleText 
+          title={<>Get Started with just a few clicks</>}
+        />
+        <div
+        className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]"
+      >
+        {startingFeatures.map((feature, index) => (
+          <StartSteps 
+            key={feature}
+            number={index + 1}
+            title={feature}
+          />
+        ))}
+      </div>
       </motion.div>
     </motion.div>
   </section>
