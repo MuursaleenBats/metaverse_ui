d5c53ea95ee4fa81cbe5873c7394cd0a1a1a86c8
MuursaleenBats <mursaleenbatatawala@gmail.com>
2023-09-13 23:29:23 +0530

    world section

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,31 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from '../styles';
+import {staggerContainer, fadeIn} from '../utils/motion';
+import {NewFeatures, TitleText, TypingText } from "../components";
+
 const World = () => (
-  <section>
-    World section
-  </section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView='show'
+      viewport={{once: false, amount:0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText 
+        title="| People on the world"
+        textStyles="text-center"
+      />
+      <TitleText 
+        title={<>
+          Track friends around you and invite them to play together in the same world
+        </>}
+        textStyles="text-center"
+      />
+    </motion.div>  
+    </section>
 );
 
 export default World;
